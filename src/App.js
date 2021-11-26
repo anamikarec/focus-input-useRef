a94ec7ea4ab1e76825f07fc14eae2344456eecd7
Anamika <anamikarecsonbhadra@gmail.com>
2021-11-26 12:56:55 +0530

    StopWatch with Start,pause and Stop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState, useEffect, useRef } from 'react';
 function App() {
+  const [time,setTime] = useState(0);
+  const timerRef = useRef(null);
+
+  useEffect(()=>{
+    return pauseTimer;
+  },[])
+  const startTimer = () =>{
+    if(timerRef.current === null){
+      timerRef.current = setInterval(()=>{
+        setTime((prev) => prev+1);
+      },1000)
+    }
+  }
+  const pauseTimer = () =>{
+    clearInterval(timerRef.current);
+    timerRef.current = null;
+  }
+  const resetTimer = () =>{
+    pauseTimer();
+    setTime(0);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Stopwatch</h1>
+      <h3>{time}</h3>
+      <button className="btn" onClick={startTimer}>START</button>
+      <button className="btn" onClick={pauseTimer}>PAUSE</button>
+      <button className="btn" onClick={resetTimer}>RESET</button>
     </div>
   );
 }
